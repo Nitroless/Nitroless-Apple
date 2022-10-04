402888be7783eada1e6ad839faf5fbae1a89b936
llsc12 <42747613+llsc12@users.noreply.github.com>
2022-10-04 22:53:07 +0100

    kb foundation work

--- Keyboard/KeyboardView.swift
+++ Keyboard/KeyboardView.swift
@@ -7,8 +7,11 @@
 
 import Foundation
 import SwiftUI
+import SDWebImageSwiftUI
 
 struct KeyboardView: View {
+    
+    @Environment(\.colorScheme) var cs
     var vc: KeyboardViewController
     
     lazy var showGlobe: Bool = {
@@ -17,16 +20,56 @@ struct KeyboardView: View {
     }()
     
     var body: some View {
+        // kb needs full access
+        // thanks emily
+        
         VStack {
-            Text("hi mom")
-                .onTapGesture {
-                    vc.textDocumentProxy.insertText("hi mom")
-                }
-            
-            Button("E") {
-                vc.textDocumentProxy.insertText("E")
+            if vc.hasFullAccess {
+                kb
+            } else {
+                askForAccess
             }
-            .buttonStyle(.bordered)
         }
+        .background {
+            let discordBgDark = Color(red: 0.2156, green: 0.2235, blue: 0.2431)
+            let discordBgLight = Color(red: 1, green: 1, blue: 1)
+            Rectangle()
+                .foregroundColor(cs == .light ? discordBgLight : discordBgDark)
+        }
+    }
+    
+    @ViewBuilder
+    var kb: some View {
+        HStack {
+            VStack {
+                Text("hi mom")
+                    .onTapGesture {
+                        vc.textDocumentProxy.insertText("hi mom")
+                    }
+                
+                Button("E") {
+                    vc.textDocumentProxy.insertText("E")
+                }
+                .buttonStyle(.bordered)
+            }
+            
+            let mogus = URL(string: "https://cdn.discordapp.com/emojis/820174282037657620.gif?size=48")!
+            WebImage(url: mogus)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .onTapGesture {
+                    type(mogus.absoluteString)
+                }
+        }
+    }
+    
+    @ViewBuilder
+    var askForAccess: some View {
+        Text("Heya, Nitroless Keyboard requires full keyboard access.\nSettings > General > Keyboards > Keyboard - Nitroless > Allow Full Access")
+//            .frame(minHeight: 400)
+    }
+    
+    func type(_ str: String) {
+        vc.textDocumentProxy.insertText(str)
     }
 }
