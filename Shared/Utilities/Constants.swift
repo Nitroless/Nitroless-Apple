be30fc2f506c1572d2616bfff3176e546b5a5487
Lakhan Lothiyi <42747613+llsc12@users.noreply.github.com>
2022-11-08 17:58:54 +0000

    hmm

--- Shared/Utilities/Constants.swift
+++ Shared/Utilities/Constants.swift
@@ -8,10 +8,37 @@
 import Foundation
 
 struct FileLocations {
-    static let repoList = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.llsc12.Nitroless")!.appendingPathComponent("Documents").appending(path: "repos").appendingPathExtension("nitroless")
-    static let frequentEmotes = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.llsc12.Nitroless")!.appendingPathComponent("Documents").appending(path: "frequentEmotes").appendingPathExtension("nitroless")
-    static let favouriteEmotes = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.llsc12.Nitroless")!.appendingPathComponent("Documents").appending(path: "favouriteEmotes").appendingPathExtension("nitroless")
+    static var repoList: URL = {
+        let root = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.llsc12.Nitroless")!
+        var str = root.absoluteString + "Documents/repos.nitroless"
+        str=String(str.dropFirst(7))
+        let final = URL(string: str)!
+        return final
+    }()
+    static var frequentEmotes: URL = {
+        let root = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.llsc12.Nitroless")!
+        var str = root.absoluteString + "Documents/frequentEmotes.nitroless"
+        str=String(str.dropFirst(7))
+        let final = URL(string: str)!
+        return final
+    }()
+    static var favouriteEmotes: URL = {
+        let root = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.llsc12.Nitroless")!
+        var str = root.absoluteString + "Documents/favouriteEmotes.nitroless"
+        str=String(str.dropFirst(7))
+        let final = URL(string: str)!
+        return final
+    }()
 }
 
 var DefaultReposUrl = URL(string: "https://nitroless.app/default.json")!
+
+extension URL {
+    func appending(componentThing: String) {
+        let a = componentThing
+        let str = self.absoluteString
+        let urlcomp = self.host
+        
+    }
+}
 
