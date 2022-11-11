b5197389fa3ec7112d452c2d16f29010293db271
Lakhan Lothiyi <42747613+llsc12@users.noreply.github.com>
2022-11-11 11:29:24 +0000

    temp fix

---
+++ Shared/Utilities/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  Nitroless
+//
+//  Created by Lakhan Lothiyi on 10/11/2022.
+//
+
+import Foundation
+
+let DefaultReposUrl = URL(string: "https://nitroless.app/default.json")!
--- Shared/Utilities/Storage/StorageManager.swift
+++ Shared/Utilities/Storage/StorageManager.swift
@@ -24,21 +24,56 @@ class StorageManager: ObservableObject {
     
     var s: StorageObject {
         didSet {
-            self.objectWillChange.send()
-            StorageManager.saveLoadedObjectToLocalStorage(self)
+            if self.pauseSaves == false {
+                self.objectWillChange.send()
+                StorageManager.saveLoadedObjectToLocalStorage(self)
+            }
         }
     }
     
+    var timerRefresher: Timer? = nil
+    var pauseSaves = false
+    
     init() {
+        
+        // create file if it doesnt exist
+        if !FileManager.default.fileExists(atPath: fileLocation.path) {
+            let starterObj = StorageObject(repos: [], frequentEmotes: [], favouriteEmotes: [], favouriteEmotesPerRepo: [:])
+            let json = try? encoder.encode(starterObj)
+            try? json?.write(to: fileLocation, options: .atomic)
+            
+            // else if it does exist, make sure it successfully gets loaded and decodes, else write a blank template.
+        } else if let data = try? Data(contentsOf: fileLocation),
+                  let _ = try? decoder.decode(StorageObject.self, from: data) {} else {
+                      let starterObj = StorageObject(repos: [], frequentEmotes: [], favouriteEmotes: [], favouriteEmotesPerRepo: [:])
+                      let json = try? encoder.encode(starterObj)
+                      try? json?.write(to: fileLocation, options: .atomic)
+                  }
+        
         self.s = StorageManager.getObjectFromLocalStorage(decoder, fileLocation, encoder: encoder)
+        
+        self.timerRefresher = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { _ in
+            self.pauseSaves = true
+            StorageManager.reloadObjectFromLocalStorage(self)
+            self.pauseSaves = false
+        })
+        
         self.objectWillChange.send()
+    }
+    
+    deinit {
+        self.timerRefresher?.invalidate()
+    }
+    
+    static private func reloadObjectFromLocalStorage(_ self: StorageManager) {
+        self.s = StorageManager.getObjectFromLocalStorage(self.decoder, self.fileLocation, encoder: self.encoder)
     }
     
     static private func saveLoadedObjectToLocalStorage(_ self: StorageManager) {
         print("saving object")
         do {
-            let json = try self.encoder.encode(self.s)
-            try json.write(to: self.fileLocation, options: .atomic)
+                let json = try self.encoder.encode(self.s)
+                try json.write(to: self.fileLocation, options: .atomic)
         } catch {
             fatalError(error.localizedDescription)
         }
